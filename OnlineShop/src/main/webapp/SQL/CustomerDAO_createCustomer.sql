3cb7feffb6a776e72ccafd1cf1fc3990f1825b80
Yusukee <96870513+YusukeTeo@users.noreply.github.com>
2022-11-08 16:00:14 +0900

    Update CustomerDAO_createCustomer.sql

--- OnlineShop/src/main/webapp/SQL/CustomerDAO_createCustomer.sql
+++ OnlineShop/src/main/webapp/SQL/CustomerDAO_createCustomer.sql
@@ -1,7 +1,7 @@
 create table customers(
-	id SERIAL PRIMARY KEY,
-	name TEXT NOT NULL,
-	password TEXT NOT NULL,
+　　　id SERIAL PRIMARY KEY,
+　　　　name TEXT NOT NULL,
+　　　　password TEXT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   deleted_at TIMESTAMP
